a88b76fdf8888716e0954c037134a2b8366ebe7f
e-carlin <evan@carlin.com>
2017-06-01 16:24:16 -0700

    Added ability to sign up new user

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import SMSView
+from .views import SMSView, IndexView
 
 urlpatterns = {
     url(r'^api/v1/sms/$', SMSView.as_view(), name='sms-view'),
+    url(r'^$', IndexView.as_view(), name='index-view'),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView # For SMS
 from django.http import HttpResponse # For SMS
-from rest_framework.response import Response # For GET
+from django.views import View
 from .serializers import UserSerializer 
 from .sms_parser import respond_to_request
+# ------ For IndexView --------
+from rest_framework.response import Response # For GET
+from django.http import HttpResponse # For index
+from django.shortcuts import render
+from .models import User
+from .forms import UserForm
 
 
 class SMSView(APIView):
@@ -13,3 +19,20 @@ class SMSView(APIView):
 
 	def get(self, request, *args, **kwargs):
 	    return Response("You got me")
+
+
+class IndexView(View):
+
+	def get(self, request, *args, **kwargs):
+		# users = User.objects.all()
+		# return render(request, 'web_interface/index.html', {'users': users})
+		form = UserForm()
+		return render(request, 'web_interface/add_user.html', {'form': form})
+
+	def post(self, request, *args, **kwargs):
+		form = UserForm(request.POST)
+		if form.is_valid():
+			post = form.save()
+			return HttpResponse("You have been added to the db")
+		else:
+			return HttpResponse("Woops something went wrong!")
