57e63085c5f81bd73d4f664df7814047ec19f80e
e-carlin <evan@carlin.com>
2017-06-01 15:33:42 -0700

    Simplified get

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView # For SMS
 from django.http import HttpResponse # For SMS
+from rest_framework.response import Response # For GET
 from .serializers import UserSerializer 
 from .sms_parser import respond_to_request
 
@@ -11,7 +12,4 @@ class SMSView(APIView):
 
 
 	def get(self, request, *args, **kwargs):
-	    print(" ***** SMS Got a GET")
-	    resp = MessagingResponse()
-	    resp.message("WHAT, Why was a GET triggered!!????")
-	    return HttpResponse(str(resp))
+	    return Response("You got me")
