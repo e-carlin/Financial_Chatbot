6d86a59ddd6bfcd0102a15098b8fff0ffabae079
e-carlin <evan@carlin.com>
2017-06-01 15:28:06 -0700

    try again..

--- website/settings.py
+++ website/settings.py
@@ -149,10 +149,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # ****************************************************************************
