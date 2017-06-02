fe7fbbbdf0c73f9b8dd67ade23c4e4386951815f
e-carlin <evan@carlin.com>
2017-06-02 07:42:47 -0700

    new db

--- website/production_settings.py
+++ website/production_settings.py
@@ -1,5 +1,5 @@
 import dj_database_url
 
-DATABASES = {'default': dj_database_url.config(default="postgres://horppihyabccmv:914fa0a2e87471f9b408799f0432a6a96d532d8eea8a15fe3e957f62b65fd61b@ec2-54-225-68-71.compute-1.amazonaws.com:5432/d112fanfi0cnrf")}
+DATABASES['default'] =  dj_database_url.config()
 
 DEBUG = True # For now very insecure for prod TODO: Make False
